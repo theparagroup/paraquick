d79419dd1dad7ab524066baae0ccbf8a6bb60cc3
Joe Barnett <joebarn@users.noreply.github.com>
2017-06-10 22:10:51 -0400

    initial commit

---
+++ paraquick-qbwc/Service.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services;
+
+namespace com.paralib.paraquick.qbwc
+{
+    [WebService(Namespace = "http://developer.intuit.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public abstract class Service: WebService
+    {
+        [WebMethod()]
+        public abstract string[] authenticate(string strUserName, string strPassword);
+
+        [WebMethod()]
+        public abstract string clientVersion(string strVersion);
+
+        [WebMethod()]
+        public abstract string serverVersion();
+
+        [WebMethod()]
+        public abstract string closeConnection(string ticket);
+
+        [WebMethod()]
+        public abstract string connectionError(string ticket, string hresult, string message);
+
+        [WebMethod()]
+        public abstract string getLastError(string ticket);
+
+        [WebMethod()]
+        public abstract string sendRequestXML(string ticket, string strHCPResponse, string strCompanyFileName, string qbXMLCountry, int qbXMLMajorVers, int qbXMLMinorVers);
+
+        [WebMethod()]
+        public abstract int receiveResponseXML(string ticket, string response, string hresult, string message);
+
+        [WebMethod()]
+        public abstract string getInteractiveURL(string wcTicket, string sessionID);
+
+        [WebMethod()]
+        public abstract string interactiveDone(string wcTicket);
+
+        [WebMethod()]
+        public abstract string interactiveRejected(string wcTicket, string reason);
+
+    }
+}
+
+
+
---
+++ qb-test/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using com.paralib.paraquick.qbxml;
+using com.paralib.paraquick.qbxml.Serializers;
+
+namespace qb_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerAddRqType customerAdd = new CustomerAddRqType();
+            customerAdd.CustomerAdd = new CustomerAdd();
+            customerAdd.CustomerAdd.FirstName = "John";
+            customerAdd.CustomerAdd.Name = "John Smith";
+            customerAdd.CustomerAdd.LastName = "Smith";
+
+            QBXMLMsgsRq request = new QBXMLMsgsRq();
+            request.onError = QBXMLMsgsRqOnError.stopOnError;
+            request.Items = new[] { customerAdd };
+
+            QBXML qbxml = new QBXML() { Items = new[] { request }, ItemsElementName = new[] { ItemsChoiceType99.QBXMLMsgsRq } };
+
+            XmlWriterSettings xs = new XmlWriterSettings
+            {
+                Indent = true,
+                OmitXmlDeclaration = false,
+                Encoding = Encoding.UTF8
+
+
+            };
+
+            string xml;
+
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (var xw = XmlWriter.Create(ms, xs))
+                {
+                    //XmlSerializer ser = new XmlSerializer(typeof(QBXML));
+
+                    var ser = new QBXMLSerializer();
+                    XmlSerializerNamespaces ns = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+                    ser.Serialize(xw, qbxml, ns);
+
+                    //var ser = new Microsoft.Xml.Serialization.GeneratedAssembly.CustomerAddSerializer();
+                    //XmlSerializerNamespaces ns = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+                    //ser.Serialize(xw, customerAdd.CustomerAdd, ns);
+
+                }
+
+
+                ms.Position = 0;
+
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(ms);
+
+                XmlProcessingInstruction pi;
+                pi = xmlDoc.CreateProcessingInstruction("qbxml", "version=\"13.0\"");
+                xmlDoc.InsertAfter(pi, xmlDoc.FirstChild);
+
+
+                using (MemoryStream ms2 = new MemoryStream())
+                {
+                    xmlDoc.Save(ms2);
+
+                    ms2.Position = 0;
+
+                    using (StreamReader sr = new StreamReader(ms2))
+                    {
+                        xml = sr.ReadToEnd();
+                    }
+                }
+
+                //xml = xmlDoc.OuterXml;
+
+                //using (StreamReader sr = new StreamReader(ms))
+                //{
+                //    xml = sr.ReadToEnd();
+                //}
+
+            }
+
+            Console.WriteLine(xml);
+            File.WriteAllText("addcustomer.xml", xml);
+        }
+    }
+}
